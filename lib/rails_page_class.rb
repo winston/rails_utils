36e07ffcae1f6483bea0cb5313041ebc0904e5e2
Winston <winston.yongwei@gmail.com>
2013-04-07 22:34:56 +0800

    Add javascript initialization too.

--- lib/rails_page_class.rb
+++ lib/rails_page_class.rb
@@ -6,6 +6,18 @@ module RailsPageClass
       "#{page_controller_class} #{page_action_class}"
     end
 
+    def javascript_initialization
+      application_name = Rails.application.class.parent_name
+
+      javascript_tag <<-JS
+        #{application_name}.init();
+        if(#{application_name}.#{page_controller_class}) {
+          if(#{application_name}.#{page_controller_class}.init) { #{application_name}.#{page_controller_class}.init(); }
+          if(#{application_name}.#{page_controller_class}.init_#{page_action_class}) { #{application_name}.#{page_controller_class}.init_#{page_action_class}(); }
+        }
+      JS
+    end
+
     private
 
     def page_controller_class
--- test/rails_page_class_test.rb
+++ test/rails_page_class_test.rb
@@ -7,25 +7,65 @@ describe "RailsPageClass::ActionViewExtensions" do
 
   before { view.controller = controller }
 
-  # controller_name, action_name, expected
-  [
-    [ "anime", "index"  , "anime index"   ],
-    [ "anime", "show"   , "anime show"    ],
-    [ "anime", "new"    , "anime new"     ],
-    [ "anime", "create" , "anime new"     ],
-    [ "anime", "edit"   , "anime edit"    ],
-    [ "anime", "update" , "anime edit"    ],
-    [ "anime", "destroy", "anime destroy" ],
-    [ "anime", "custom" , "anime custom"  ],
-  ].each do |controller_name, action_name, expected|
-    describe "when #{controller_name} and #{action_name}" do
-      before do
-        controller.stubs(:controller_name).returns(controller_name)
-        controller.stubs(:action_name).returns(action_name)
+  describe "#page_class" do
+    # controller_name, action_name, expected
+    [
+      [ "anime", "index"  , "anime index"   ],
+      [ "anime", "show"   , "anime show"    ],
+      [ "anime", "new"    , "anime new"     ],
+      [ "anime", "create" , "anime new"     ],
+      [ "anime", "edit"   , "anime edit"    ],
+      [ "anime", "update" , "anime edit"    ],
+      [ "anime", "destroy", "anime destroy" ],
+      [ "anime", "custom" , "anime custom"  ],
+    ].each do |controller_name, action_name, expected|
+      describe "when #{controller_name} and #{action_name}" do
+        before do
+          controller.stubs(:controller_name).returns(controller_name)
+          controller.stubs(:action_name).returns(action_name)
+        end
+
+        it "returns #{expected}" do
+           view.page_class.must_equal expected
+        end
+      end
+    end
+  end
+
+  describe "#javascript_initialization" do
+    let(:controller_name) { "anime" }
+
+    before do
+      controller.stubs(:controller_name).returns(controller_name)
+      controller.stubs(:action_name).returns(action_name)
+    end
+
+    describe "when controller name and action name are standard" do
+      let(:action_name)     { "custom" }
+
+      it "invokes application" do
+        view.javascript_initialization.must_match "Dummy.init();"
       end
 
-      it "returns #{expected}" do
-         view.page_class.must_equal expected
+      it "invokes controller and action javascript" do
+        view.javascript_initialization.must_match "Dummy.#{controller_name}.init();"
+        view.javascript_initialization.must_match "Dummy.#{controller_name}.init_#{action_name}();"
+      end
+    end
+
+    describe "when action name is create" do
+      let(:action_name)     { "create" }
+
+      it "replaces create with new" do
+        view.javascript_initialization.must_match "Dummy.#{controller_name}.init_new();"
+      end
+    end
+
+    describe "when action name is update" do
+      let(:action_name)     { "update" }
+
+      it "replaces update with create" do
+        view.javascript_initialization.must_match "Dummy.#{controller_name}.init_edit();"
       end
     end
   end
